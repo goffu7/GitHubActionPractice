4974a97971f66367aef984b3ef2ef44891ef8997
goffu7 <135923234+goffu7@users.noreply.github.com>
2024-04-24 20:31:00 +0700

    update app.test.js

--- app.test.js
+++ app.test.js
@@ -6,7 +6,23 @@ test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('3/11/2020') );
     expect( day ).toBe('Wednesday');
 });
+
+test('getDay returns the long-format day of the week', () => {
+    const day = dayOfTheWeek( new Date('3/12/2020') );
+    expect( day ).toBe('Thursday');
+});
+
 test('getMonth returns the month of the year', () => {
     const month = monthOfTheYear( new Date('3/11/2020') );
-    expect( month ).toBe('November');
+    expect( month ).toBe('March');
+});
+
+test('getMonth returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('4/11/2020') );
+    expect( month ).toBe('April');
+});
+
+test('getMonth returns the month of the year', () => {
+    const month = monthOfTheYear( new Date('5/11/2020') );
+    expect( month ).toBe('May');
 });
